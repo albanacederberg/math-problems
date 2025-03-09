64f0fe2411285f86d5b88498f1eb68ab2027c351
albanacederberg <albanacederberg@gmail.com>
2025-03-09 19:35:30 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,9 @@
+// A simple program to solve a math problem
+#include <stdio.h>
+
+int main() {
+  int number1 = 5;
+  int number2 = 3;
+  int sum = number1 + number2;
+  printf("The sum of %d and %d is %d", number1, number2, sum);
+}
