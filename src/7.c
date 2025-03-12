0fb79ece1a1e2297717056057029b96a7bebca13
albanacederberg <albanacederberg@gmail.com>
2025-03-12 02:04:41 +0000

    Add 7.c

---
+++ src/7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    int a = 5;
+    int b = 8;
+    double c = (a + b) / 2;
+    printf("The average of %d and %d is %.1f\n", a, b, c);
+    return 0;
+}
