6bbd84d5ac071a7dbb44f9727a34c4cc5e8dc598
albanacederberg <albanacederberg@gmail.com>
2025-05-26 04:17:29 +0000

    Add 45.c

---
+++ src/45.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    int result;
+
+    result = num1 + num2; // Sum of num1 and num2
+
+    printf("The sum of %d and %d is: %d\n", num1, num2, result);
+
+    return 0;
+}
